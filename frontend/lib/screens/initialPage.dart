91710747d4a20d9b4623fa53c70b4506920a2dc7
Minsoo Kim <ms214@ms214.kr>
2025-03-26 14:12:50 +0900

    feat: 라우팅 코드 추가 #1

---
+++ frontend/lib/screens/initialPage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Initialpage extends StatelessWidget {
+  const Initialpage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+            "메인페이지입니다."
+        ),
+      ),
+    );
+  }
+}
