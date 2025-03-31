65235d9ca7a7d92ccbdf63510f87f05e9676a2bd
Minsoo Kim <ms214@ms214.kr>
2025-03-31 23:10:33 +0900

    feat: 기본 폰트 추가 #1

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -29,8 +29,9 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
         useMaterial3: true,
+        fontFamily: 'Pretendard',
       ),
       initialRoute: Routes.splash,
       routes: namedRoute,
