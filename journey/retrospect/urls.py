dd4bfe26b48493ea1df071b103aeb7e4111831f9
sungminna <sungmin.na330@gmail.com>
2025-04-07 15:06:20 +0900

    fix : retrospect weekely analysis api

--- journey/retrospect/urls.py
+++ journey/retrospect/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import RetrospectViewSet, TemplateViewSet, ChallengeViewSet
+from .views import RetrospectViewSet, TemplateViewSet, ChallengeViewSet, RetrospectWeeklyAnalysisViewSet
 
 router = DefaultRouter()
 router.register(r'retrospects', RetrospectViewSet, basename='retrospect')
 router.register(r'templates', TemplateViewSet, basename='template')
 router.register(r'challenges', ChallengeViewSet, basename='challenge')
+router.register(r'retrospect-weekly-analysis', RetrospectWeeklyAnalysisViewSet, basename='retrospect-weekly-analysis')
 
 urlpatterns = [
     path('', include(router.urls)),
