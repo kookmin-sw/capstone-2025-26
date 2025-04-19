20875acf50c57d8c055bc937cde1144948ba8b9a
Minsoo Kim <ms214@ms214.kr>
2025-04-19 13:04:57 +0900

    feat: 피드 페이지 생성 #30
    사용자 프로필사진, 이름, 팔로우버튼을 제외하고

--- frontend/lib/icon/tab_bar_icon_icons.dart
+++ frontend/lib/icon/tab_bar_icon_icons.dart
@@ -21,9 +21,11 @@ class TabBarIcon {
   static const _kFontFam = 'TabBarIcon';
   static const String? _kFontPkg = null;
 
+  static const IconData comment = IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData award = IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData home = IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData layers = IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData user = IconData(0xe804, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData command = IconData(0xe805, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData heart = IconData(0xe806, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 }
---
+++ frontend/lib/screens/feed.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:reme/icon/tab_bar_icon_icons.dart';
+import 'package:reme/themes/color.dart';
+import 'package:reme/widgets/customListItem.dart';
+import 'package:reme/widgets/widgetBox.dart';
+
+class Feed extends StatefulWidget {
+  const Feed({super.key});
+
+  @override
+  State<Feed> createState() => _FeedState();
+}
+
+class _FeedState extends State<Feed> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Row(
+            children: [
+
+            ],
+          ), // 탭 메뉴. 고정 필요?
+          Container(
+            padding: EdgeInsets.all(10),
+            child: Column(
+              children: [
+                WidgetBox(
+                    children: [
+                      // 사용자 프로필사진, 이름, 팔로우 버튼 여기에
+                      
+                      CustomListitem(
+                          height: 46,
+                          content: "모두를 위한 머신러닝 읽기"
+                      ),
+                      CustomListitem(
+                          height: 46,
+                          content: "모두를 위한 머신러닝 읽기"
+                      ),
+                      CustomListitem(
+                          height: 46,
+                          content: "모두를 위한 머신러닝 읽기"
+                      ),
+                      Container(
+                        padding: EdgeInsets.fromLTRB(10, 17, 0, 0),
+                        child: Row(
+                          spacing: 14,
+                          children: [
+                            Icon(TabBarIcon.heart, color: fontColor, size: 20,),
+                            Icon(TabBarIcon.comment, color: fontColor, size: 20,)
+                          ],
+                        ),
+                      )
+                    ],
+                    isMore: false,
+                    marginLTRB: EdgeInsets.only(bottom: 10)
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
