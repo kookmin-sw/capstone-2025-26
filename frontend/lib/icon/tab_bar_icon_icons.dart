4c725073965d428d785055e8c80d45516df2f0db
Minsoo Kim <ms214@ms214.kr>
2025-04-19 23:31:28 +0900

    chore: 얇은 버전으로 아이콘 변경 #30

--- frontend/lib/icon/tab_bar_icon_icons.dart
+++ frontend/lib/icon/tab_bar_icon_icons.dart
@@ -27,10 +27,10 @@ class TabBarIcon {
 
   static const IconData comment = IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData award = IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData home = IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData layers = IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData user = IconData(0xe804, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData command = IconData(0xe805, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData heart = IconData(0xe806, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData home = IconData(0xe808, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData search = IconData(0xe809, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData command = IconData(0xe80a, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData layers = IconData(0xe80b, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData user = IconData(0xe80c, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 }
