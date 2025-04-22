b238d64184099b81ff06d35d3c69d9405a452571
KIM SUNWOO <seonu2001@kookmin.ac.kr>
2025-04-22 14:40:39 +0900

    #44 fix: user, 공지 라우터 명시적 분리

--- journey/user_manager/urls.py
+++ journey/user_manager/urls.py
@@ -2,11 +2,16 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import UserViewSet, NotificationViewSet, UserChallengeStatusView
 
-router = DefaultRouter()
-router.register(r'', UserViewSet, basename='user')
-router.register(r'notifications', NotificationViewSet, basename='notification')
+# 명확하게 분리된 라우터 생성
+user_router = DefaultRouter()
+user_router.register(r'users', UserViewSet, basename='user')
+
+notification_router = DefaultRouter()
+notification_router.register(r'notifications', NotificationViewSet, basename='notification')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    # 각 라우터를 독립적으로 포함
+    path('', include(user_router.urls)),
+    path('', include(notification_router.urls)),
     path('my-challenges/', UserChallengeStatusView.as_view(), name='my-challenges'),
 ]
