2943f6b8fbb94429e18f93a0b7bdd4cbc0ac4531
Minsoo Kim <ms214@ms214.kr>
2025-04-30 19:57:59 +0900

    fix: 크루 상세페이지 위젯간 위치 수정2 #30

--- frontend/lib/screens/crewDetail.dart
+++ frontend/lib/screens/crewDetail.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg_provider/flutter_svg_provider.dart';
 import 'package:reme/themes/color.dart';
 import 'package:reme/icon/tab_bar_icon_icons.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CrewDetail extends StatefulWidget {
   const CrewDetail({super.key});
@@ -14,7 +15,7 @@ class _CrewDetailState extends State<CrewDetail>
     with SingleTickerProviderStateMixin {
   TabController? _tabController;
   ScrollController? _scrollController;
-  final double _expandedHeight = 198+174+21;
+  final double _expandedHeight = 240.h+178.h;
   int _selectIndex = 0;
   bool _isCollapsed = false;
 
@@ -40,6 +41,7 @@ class _CrewDetailState extends State<CrewDetail>
   Widget build(BuildContext context) {
     var crewid = ModalRoute.of(context)!.settings.arguments;
 
+
     return Scaffold(
       backgroundColor: background,
       body: NestedScrollView(
@@ -47,15 +49,16 @@ class _CrewDetailState extends State<CrewDetail>
         headerSliverBuilder: (context, innerBoxIsScrolled) {
           return [
             SliverAppBar(
-              toolbarHeight: 84,
+              toolbarHeight: 84.h,
               primary: false,
+              leadingWidth: 52,
               leading: GestureDetector(
                 onTap: (){
                   Navigator.pop(context);
                 },
                 child: Container(
-                  margin: EdgeInsets.only(top: 46),
-                    child: Icon(TabBarIcon.leftArrow, size: 20,)
+                  margin: EdgeInsets.only(top: 45),
+                    child: Icon(TabBarIcon.leftArrow, size: 18,)
                 ),
               ),
               expandedHeight: _expandedHeight,
@@ -125,21 +128,21 @@ class _CrewDetailState extends State<CrewDetail>
                             clipBehavior: Clip.none,
                             children: [
                               Container(
-                                height: 170,
-                                margin: const EdgeInsets.only(top: 27),
+                                width: double.maxFinite,
+                                height: 178.h,
+                                margin: EdgeInsets.only(top: 31.h),
                                 color: boxBackgroundColor,
-                                padding:
-                                    const EdgeInsets.fromLTRB(21, 0, 21, 0),
+                                padding: EdgeInsets.fromLTRB(21.w, 0, 21.w, 0),
                                 child: Stack(
                                   children: [
                                     Positioned(
                                       child: Icon(Icons.more_vert,
                                           color: Colors.white),
-                                      right: -9,
-                                      top: 20,
+                                      right: -9.w,
+                                      top: 20.h,
                                     ),
                                     Container(
-                                    padding: const EdgeInsets.only(top: 21),
+                                    padding: EdgeInsets.only(top: 26.h),
                                     child: Column(
                                       crossAxisAlignment:
                                       CrossAxisAlignment.start,
@@ -149,27 +152,25 @@ class _CrewDetailState extends State<CrewDetail>
                                           CrossAxisAlignment.start,
                                           mainAxisSize: MainAxisSize.min,
                                           children: [
-                                            SizedBox(height: 4,),
                                             Container(
-                                              margin: EdgeInsets.only(top: 4),
-                                              child: const Row(
+                                              child: Row(
                                                 children: [
                                                   Text(
                                                     '저속 노화 따라가기',
                                                     style: TextStyle(
-                                                        fontSize: 19,
+                                                        fontSize: 19.sp,
                                                         fontWeight:
                                                         FontWeight.w800,
                                                         color: Colors.white,
                                                         height: 1.5),
                                                   ),
-                                                  SizedBox(width: 15),
+                                                  SizedBox(width: 13.w),
                                                   Icon(Icons.person_outline_outlined, color: Color(0xFF898989), size: 15,),
-                                                  SizedBox(width: 3),
+                                                  SizedBox(width: 1.w),
                                                   Text(
                                                     '12명',
                                                     style: TextStyle(
-                                                        fontSize: 12,
+                                                        fontSize: 12.sp,
                                                         color: Colors.white70),
                                                   ),
                                                   Spacer(),
@@ -178,21 +179,22 @@ class _CrewDetailState extends State<CrewDetail>
                                               ),
                                             ),
                                             Container(
-                                              child: const Text(
+                                              padding: EdgeInsets.only(top: 6.h),
+                                              child: Text(
                                                 '저속 노화 위주의 식사와 규칙적인 생활을 통해 삶을 재정비하고 이다현보다 오래 살기 위해 노력합니다',
                                                 style: TextStyle(
-                                                    fontSize: 14,
+                                                    fontSize: 14.sp,
                                                     fontWeight: FontWeight.w500,
                                                     color: fontColor,
-                                                    height: 1.70
+                                                    height: 1.70.h
                                                 ),
                                                 maxLines: 2,
                                                 overflow: TextOverflow.clip,
                                               ),
                                             ),
                                             Container(
-                                              margin: EdgeInsets.only(top: 11),
-                                              height: 40,
+                                              margin: EdgeInsets.only(top: 16.h),
+                                              height: 41.h,
                                               width: double.maxFinite,
                                               child: ElevatedButton(
                                                 style: ElevatedButton.styleFrom(
@@ -204,12 +206,12 @@ class _CrewDetailState extends State<CrewDetail>
                                                   padding: const EdgeInsets.only(top: 8, bottom: 8),
                                                 ),
                                                 onPressed: () {},
-                                                child: const Text('크루 가입하기',
+                                                child: Text('크루 가입하기',
                                                     style: TextStyle(
-                                                        fontSize: 16,
+                                                        fontSize: 16.sp,
                                                         color: fontColor,
                                                         fontWeight: FontWeight.w700,
-                                                        height: 1.5)),
+                                                        height: 1.5.h)),
                                               ),
                                             ),
                                           ],
@@ -222,15 +224,15 @@ class _CrewDetailState extends State<CrewDetail>
                               ),
                               // 아이콘만 따로 위로 올림
                               Positioned(
-                                top: -16,
-                                left: 20,
+                                top: -18.h,
+                                left: 21.w,
                                 child: Opacity(
                                   opacity: 1-t,
                                   child: ClipRRect(
                                     borderRadius: BorderRadius.circular(10),
                                     child: Container(
-                                      width: 66,
-                                      height: 66,
+                                      width: 70.w,
+                                      height: 70.h,
                                       color: Colors.white,
                                       child: Image.network(
                                         'https://your-crew-icon-url.com/icon.png',
@@ -273,12 +275,12 @@ class _CrewDetailState extends State<CrewDetail>
                                   ),
                                 ),
                               ),
-                              const SizedBox(width: 8),
+                              SizedBox(width: 8.w),
                               Text(
                                 '저속 노화 따라가기',
                                 style: TextStyle(
                                   color: Colors.white,
-                                  fontSize: nameFont,
+                                  fontSize: nameFont.sp,
                                   fontWeight: FontWeight.bold,
                                 ),
                               ),
@@ -331,6 +333,7 @@ class _CrewDetailState extends State<CrewDetail>
   Widget _buildTab(String label, bool selected) {
     return Tab(
       child: Container(
+        width: 101.w,
         decoration: ShapeDecoration(
           color: selected ? c900 : boxBackgroundColor,
           shape: RoundedRectangleBorder(
@@ -338,11 +341,12 @@ class _CrewDetailState extends State<CrewDetail>
           ),
         ),
         child: Container(
-          padding: const EdgeInsets.fromLTRB(36, 8, 36, 8),
+          padding: const EdgeInsets.fromLTRB(23, 9, 23, 9),
           child: Text(
             label,
-            style: const TextStyle(
-                fontSize: 16, fontWeight: FontWeight.w500, color: fontColor),
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontSize: 15.sp, fontWeight: FontWeight.w500, color: fontColor),
           ),
         ),
       ),
@@ -392,7 +396,7 @@ class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
   Widget build(
       BuildContext context, double shrinkOffset, bool overlapsContent) {
     return Container(
-      margin: const EdgeInsets.only(left: 10),
+      margin: const EdgeInsets.only(left: 15),
       padding: const EdgeInsets.fromLTRB(0, 5, 0, 4), // 여기서 공간 확보
       color: background,
       child: SizedBox(
@@ -426,34 +430,34 @@ class PostCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: boxBackgroundColor,
-      margin: const EdgeInsets.only(bottom: 10),
+      margin: EdgeInsets.only(bottom: 10.h),
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 21, vertical: 14),
+        padding: EdgeInsets.fromLTRB(21.w, 16.h, 15.w, 11.h),
+        // padding: EdgeInsets.symmetric(horizontal: 21.w, vertical: 14.h),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Row(
               children: [
-                const CircleAvatar(
-                  radius: 20,
+                CircleAvatar(
+                  radius: 20.r,
                   foregroundImage:
                       Svg('assets/img/account_circle.svg', color: Colors.white),
                 ),
-                const SizedBox(width: 8),
+                SizedBox(width: 12.w),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
                       nickname,
-                      style: const TextStyle(
+                      style: TextStyle(
                           color: Colors.white,
                           fontWeight: FontWeight.w800,
-                          fontSize: 12),
+                          fontSize: 12.sp),
                     ),
                     Text(
                       date,
-                      style:
-                          const TextStyle(color: Color(0xFFC3C3C3), fontSize: 12),
+                      style: TextStyle(color: Color(0xFFC3C3C3), fontSize: 12.sp),
                     ),
                   ],
                 ),
@@ -461,31 +465,35 @@ class PostCard extends StatelessWidget {
                 const Icon(Icons.more_vert, color: Color(0xFFD9D9D9), size: 20),
               ],
             ),
-            const SizedBox(height: 10),
+            SizedBox(height: 10.h),
             Text(
               title,
-              style: const TextStyle(
+              style: TextStyle(
                   color: Colors.white,
                   fontWeight: FontWeight.w700,
-                  fontSize: 20),
+                  fontSize: 20.sp,
+                  letterSpacing: 0.1.r),
             ),
-            const SizedBox(height: 3),
+            SizedBox(height: 5.h),
             Text(
               content,
-              style: const TextStyle(
+              style: TextStyle(
                   color: Colors.white,
                   fontWeight: FontWeight.w400,
-                  fontSize: 15),
+                  fontSize: 15.sp,
+                  height: 1.5,
+                  letterSpacing: 0.1.r),
             ),
-            const SizedBox(height: 14),
-            const Row(
+            SizedBox(height: 15.h),
+            Row(
               children: [
-                Icon(TabBarIcon.heart, color: Colors.white, size: 16),
-                SizedBox(width: 15),
-                Icon(TabBarIcon.comment, color: Colors.white, size: 16),
+                SizedBox(width: 1.w,),
+                Icon(TabBarIcon.heart, color: Colors.white, size: 16.sp),
+                SizedBox(width: 19.w),
+                Icon(TabBarIcon.comment, color: Colors.white, size: 19.sp),
               ],
             ),
-            SizedBox(height: 3,)
+            SizedBox(height: 10.h,)
           ],
         ),
       ),
