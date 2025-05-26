14411f0ace5f0ef1dc768ced9d564b0e236811c8
Minsoo Kim <ms214@ms214.kr>
2025-05-26 11:54:29 +0900

    feat: api 호출 주소 변경에 따른 수정 및 멤버쉽 api 연동 #109

--- frontend/lib/services/crew_api.dart
+++ frontend/lib/services/crew_api.dart
@@ -3,26 +3,38 @@ import 'package:reme/services/mydio.dart';
 MyDio dio = MyDio();
 
 Future<dynamic> getJoinedCrewList() async {
-  final response = await dio.get('/crew/my-crews/');
+  final response = await dio.get('/crew/crews/my-crews/');
   return response.data;
 }
 
 Future<dynamic> joinCrew(String crewId) async {
-  final response = await dio.post('/crew/$crewId/request-join/', null);
+  final response = await dio.post('/crew/crews/$crewId/request-join/', null);
   return response;
 }
 
 Future<dynamic> acceptJoinRequest(String crewId, String userId) async {
-  final response = await dio.post('/crew/$crewId/accept_member/$userId/', null);
+  final response =
+      await dio.post('/crew/crews/$crewId/accept_member/$userId/', null);
   return response;
 }
 
 Future<dynamic> rejectJoinRequest(String crewId, String userId) async {
-  final response = await dio.post('/crew/$crewId/reject_member/$userId/', null);
+  final response =
+      await dio.post('/crew/crews/$crewId/reject_member/$userId/', null);
   return response;
 }
 
 Future<dynamic> getCrewList() async {
-  final response = await dio.get('/crew/');
+  final response = await dio.get('/crew/crews/');
+  return response;
+}
+
+Future<dynamic> getCrewDeatil(int crewId) async {
+  final response = await dio.get('/crew/crews/${crewId.toString()}/');
+  return response;
+}
+
+Future<dynamic> getMyCrewMembership() async {
+  final response = await dio.get('/crew/memberships/my-memberships/');
   return response;
 }
