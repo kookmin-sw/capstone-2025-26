dc44298a86f57de47317350259521ff09178fe4b
Minsoo Kim <ms214@ms214.kr>
2025-05-30 11:42:00 +0900

    feat: 챌린지 목록에 챌린지명 띄우기 #127

--- frontend/lib/utils/challenge_controller.dart
+++ frontend/lib/utils/challenge_controller.dart
@@ -3,8 +3,12 @@ import 'package:get/get.dart';
 class ChallengeController {
   List<dynamic> challengeList = [].obs;
   List<dynamic> crewChallengeList = [].obs;
+  Map<String, dynamic> idchallenge = {};
 
   void setChallengeList(List<dynamic> challenge) {
     this.challengeList = challenge;
+    for (var item in challenge) {
+      idchallenge[item['id'].toString()] = item['challenge_name'];
+    }
   }
 }
